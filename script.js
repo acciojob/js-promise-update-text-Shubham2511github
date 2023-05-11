cb7e0b3b7290ec4963b036bd18b6e5370d2616c9
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-12 00:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let p = new Promise((resolve, reject)=>{
+	setTimeout({
+		resolve("Hello world!");
+	}1000);
+})
+p.then(value){
+	const val = value;
+}
+let div = document.getElementById("output");
+div.innerText = val;
